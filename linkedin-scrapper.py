ab8862172feb2e84bd4ead849dd331d4b76ed032
KhalVissi <vodz4r@gmail.com>
2022-03-04 16:29:32 +0100

    Create linledin-scrapper.py
    First working version

---
+++ linkedin-scrapper.py
@@ -0,0 +1,96 @@
+import requests
+import re
+from getpass import getpass
+
+session = requests.session()
+mobile_agent = ('Mozilla/5.0 (Linux; U; Android 4.4.2; en-us; SCH-I535 '
+                    'Build/KOT49H) AppleWebKit/534.30 (KHTML, like Gecko) '
+                    'Version/4.0 Mobile Safari/534.30')
+session.headers.update({'User-Agent': mobile_agent,
+                            'X-RestLi-Protocol-Version': '2.0.0'})
+
+anon_response = session.get('https://www.linkedin.com/login')
+login_csrf = re.findall(r'name="loginCsrfParam" value="(.*?)"', anon_response.text)
+
+email = input('\nWhat is your Linkedin email (Your account might have 10+ relations) : \n')
+password = getpass('Enter your password : \n')
+
+auth_payload = {
+        'session_key': email,
+        'session_password': password,
+        'isJsEnabled': 'false',
+        'loginCsrfParam': login_csrf
+    }
+
+response = session.post('https://www.linkedin.com/checkpoint/lg/login-submit?loginSubmitSource=GUEST_HOME', data=auth_payload, allow_redirects=False)
+
+try:
+    if response.headers['Location'] == 'https://www.linkedin.com/feed/':
+        print('Connected Successfully\n')
+    else:
+        print("Couldn't connect, wrong redirection\n")
+        exit()
+except:
+    print("Couldn't connect, credentials might be incorrect\n")
+    exit()
+
+csrf_token = session.cookies['JSESSIONID'].replace('"', '')
+session.headers.update({'Csrf-Token': csrf_token})
+
+input_name = input('Wich company ? (Name should be shown on company url, e.g : Uber=>uber-com, Apple=>apple, ...) : \n')
+
+company_response = session.get(('https://www.linkedin.com/voyager/api/organization/companies?q=universalName&universalName='+input_name))
+
+website_regex = r'companyPageUrl":"(http.*?)"'
+name_regex = r'"name":"(.*?)"'
+staff_regex = r'staffCount":([0-9]+),'
+id_regex = r'"objectUrn":"urn:li:company:([0-9]+)"'
+desc_regex = r'tagline":"(.*?)"'
+location_regex = r'"streetAddressOptOut":true,"country":"(.*?)","city":"(.*?)","headquarter":true,"description":"(.*?)"}]}]'
+
+try:
+    company_id = re.findall(id_regex, company_response.text)[0]
+except:
+    company_id = 'None'
+try:
+    company_name = re.findall(name_regex, company_response.text)[0]
+except:
+    company_name = 'None'
+try:
+    company_staff_count = re.findall(staff_regex, company_response.text)[0]
+except:
+    company_staff_count = 'None'
+try:
+    company_website = re.findall(website_regex, company_response.text)[0]
+except:
+    company_website = 'None'
+try:
+    company_description = re.findall(desc_regex, company_response.text)[0]
+except:
+    company_description = 'None'
+try:
+    company_location = re.findall(location_regex, company_response.text)[0]
+except:
+    company_location = 'None'
+
+print('COMPANY DETAILS\n')
+print('Company name : %s'%company_name)
+print('Description : %s'%company_description)
+print('Website : %s'%company_website)
+print('Location : %s : %s, %s\n'%(company_location[2],company_location[1],company_location[0]))
+print('Staff : %s'%company_staff_count)
+
+employees_response = session.get(('https://www.linkedin.com/voyager/api/search/hits?facetCurrentCompany=List('+ company_id +')&facetGeoRegion=List()&keywords=List()&q=people&maxFacetValues=15&supportedFacets=List(GEO_REGION,CURRENT_COMPANY)&count=25&origin=organization&start=0'))
+
+employee_regex = r'firstName":"(.*?)","lastName":"(.*?)","dashEntityUrn":"urn:li:fsd_profile:(.*?)","occupation":"(.*?)"'
+
+employees = re.findall(employee_regex, employees_response.text)
+
+profile_url_header = "https://www.linkedin.com/profile/view?id="
+print('_____________ Employees ______________\n')
+
+for employee in employees:
+    print('Name : %s %s'%(employee[0], employee[1]))
+    print('Role : %s'%employee[3])
+    print('Link to profile : %s%s'%(profile_url_header,employee[2]))
+    print('_________________________________\n')
