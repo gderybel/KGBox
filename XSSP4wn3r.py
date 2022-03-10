e8e869f9eecaccbe87609ea204d49eb9b349dae7
KhalVissi <vodz4r@gmail.com>
2022-03-10 10:41:55 +0100

    Update XSSP4wn3r.py

--- XSSP4wn3r.py
+++ XSSP4wn3r.py
@@ -1,7 +1,17 @@
 # Payload source : https://github.com/payloadbox/xss-payload-list/blob/master/Intruder/xss-payload-list.txt
+import requests
 
 def LoadPayloads():
     file = open('payloads.txt', 'r')
     payloads = file.readlines()
     file.close()
     return payloads
+
+def TestPayloads(payloads, target, parameter):
+    for payload in payloads:
+        #response = requests.get(target+payload)
+        print(target+payload)
+
+
+payloads = LoadPayloads()
+TestPayloads(payloads, 'https://test.com/')
