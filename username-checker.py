d4e5167afdab7272b9c0ca36bca4f45c148b9b69
KhalVissi <vodz4r@gmail.com>
2022-03-10 10:08:43 +0100

    Add user agent

--- username-checker.py
+++ username-checker.py
@@ -2,18 +2,23 @@ import requests
 
 social_media_list = {
     "Facebook":"https://www.facebook.com/",
-    "Twitter":"https://twitter.com/",#http code not working
-    "Instagram":"https://www.instagram.com/",#http code not working
-    "TikTok":"https://www.tiktok.com/@",#http code not working
-    "Twitch":"https://www.twitch.tv/",#http code not working
-    "Github":"https://github.com/"
+    "Instagram":"https://www.instagram.com/",
+    "TikTok":"https://www.tiktok.com/@",
+    "Twitch":"https://www.twitch.tv/",
+    "Github":"https://github.com/",
 }
 
 
 def findUsername(username):
+    session = requests.session()
+    user_agent = ('Mozilla/5.0 (Linux; U; Android 4.4.2; en-us; SCH-I535 '
+                        'Build/KOT49H) AppleWebKit/534.30 (KHTML, like Gecko) '
+                        'Version/4.0 Mobile Safari/534.30')
+    session.headers.update({'User-Agent': user_agent})
+
     for social_media in social_media_list.keys():
         link = social_media_list[social_media]+username
-        response = requests.get(link)
+        response = session.get(link)
         if response.status_code == 200:
             print(social_media + ': User exists -> ' + link)
         else:
